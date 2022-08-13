a4f15a10e8654e71f7d7062f2786e3e9a7dfd3e8
Balázs Mátyássy <balazs.matyassy@hotmail.com>
2022-08-13 14:25:41 +0200

    balazs-matyassy: viewPage

--- src/main/java/hu/progmatic/webpage/controller/PageController.java
+++ src/main/java/hu/progmatic/webpage/controller/PageController.java
@@ -1,14 +1,34 @@
 package hu.progmatic.webpage.controller;
 
+import hu.progmatic.webpage.model.Page;
+import hu.progmatic.webpage.repository.PageRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Optional;
 
 @Controller
 public class PageController {
 
+    private final PageRepository pageRepository;
+
+    public PageController(PageRepository pageRepository) {
+        this.pageRepository = pageRepository;
+    }
+
     @GetMapping("/")
     public String homePage() {
         return "home";
+    }
+
+    @GetMapping("/pages/{slug}")
+    public String viewPage(@PathVariable String slug, Model model) {
+        Optional<Page> page = pageRepository.findBySlug(slug);
+        model.addAttribute("page", page.orElseThrow());
+
+        return "page";
     }
 
     @GetMapping("/admin")
