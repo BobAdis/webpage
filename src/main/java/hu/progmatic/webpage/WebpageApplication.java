9d50ea663e244f00d2c4168d65a83d1d593436eb
Balázs Mátyássy <balazs.matyassy@hotmail.com>
2022-08-13 13:20:22 +0200

    Initial commit

---
+++ src/main/java/hu/progmatic/webpage/WebpageApplication.java
@@ -0,0 +1,13 @@
+package hu.progmatic.webpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebpageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebpageApplication.class, args);
+    }
+
+}
