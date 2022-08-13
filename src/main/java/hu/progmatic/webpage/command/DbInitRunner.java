bca08253ba44d273016c4e132b1a68615dff230d
Balázs Mátyássy <balazs.matyassy@hotmail.com>
2022-08-13 14:07:49 +0200

    balazs-matyassy: test data update

--- src/main/java/hu/progmatic/webpage/command/DbInitRunner.java
+++ src/main/java/hu/progmatic/webpage/command/DbInitRunner.java
@@ -5,6 +5,8 @@ import hu.progmatic.webpage.repository.PageRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+
 @Component
 public class DbInitRunner implements CommandLineRunner {
     private final PageRepository repository;
@@ -20,7 +22,11 @@ public class DbInitRunner implements CommandLineRunner {
         repository.save(new Page("products", "Products", "Hello on the products page!"));
         System.out.println("Products page created.");
 
-        repository.save(new Page("about", "About", "Hello on the about page!"));
+        repository.save(new Page(
+                null, "about", "About",
+                "Hello on the about page!",
+                LocalDateTime.now().minusDays(10))
+        );
         System.out.println("About page created.");
     }
 }
